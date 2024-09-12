7d7c782a8762893ea2f202ee0fbcf3400f0436fa
SearchingSteve <155838264+SearchingSteve@users.noreply.github.com>
2024-09-12 17:54:00 -0230

    Create PasswordV2 Branch
    Password V2 Branch includes the 2nd iteration of the password generator
    
    - Number generation capability: upon using --number the password will randomly generate numbers throughout the password

--- password-generator-cli/index.js
+++ password-generator-cli/index.js
@@ -3,17 +3,20 @@
 const { argv } = require('process');
 
 /**
- * Creates a random password of lowercase letters with a specified length.
+ * Creates a random password with a specified length and optional numbers.
  * 
  * @param {number} length Length of the password
+ * @param {boolean} includeNumbers Whether to include numbers in the password
  * @returns {string} Generated password
  */
-function generatePassword(length) {
+function generatePassword(length, includeNumbers) {
     const lowercase = 'abcdefghijklmnopqrstuvwxyz';
+    const numbers = '0123456789';
+    const characters = includeNumbers ? lowercase + numbers : lowercase;
     let password = '';
     for (let i = 0; i < length; i++) {
-        const randomLetter = lowercase[Math.floor(Math.random() * lowercase.length)];
-        password += randomLetter;
+        const randomChar = characters[Math.floor(Math.random() * characters.length)];
+        password += randomChar;
     }
     return password;
 }
@@ -28,9 +31,10 @@ Usage: password-generator [options]
 Options:
     -h, --help                    Display help message
     -l, --length <length>         Set the length of the password (default is 8)
+    -n, --numbers                 Include numbers in the password
 
 Example:
-  password-generator --length 12
+  password-generator --length 12 --numbers
     `);
 }
 
@@ -41,6 +45,7 @@ Example:
  */
 function handleArguments(userArguments) {
     let length = 8; // Default password length
+    let includeNumbers = false;
 
     for (let i = 0; i < userArguments.length; i++) {
         switch (userArguments[i]) {
@@ -56,11 +61,15 @@ function handleArguments(userArguments) {
                     return;
                 }
                 break;
+            case '-n':
+            case '--numbers':
+                includeNumbers = true;
+                break;
         }
     }
 
     // Generate and display password
-    const password = generatePassword(length);
+    const password = generatePassword(length, includeNumbers);
     console.log(password);
 }
 
