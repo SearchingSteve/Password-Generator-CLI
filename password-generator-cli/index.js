5ba7fb0d29bd8bae0be352ac71e817dfe1e03d23
SearchingSteve <155838264+SearchingSteve@users.noreply.github.com>
2024-09-12 18:02:26 -0230

    Password Generator V3
    Password Generator V3 now contains 2 more possible parameters:
    
    --capitals which will include capitals throughout the password
    --symbols which will include symbols throughout the password

--- password-generator-cli/index.js
+++ password-generator-cli/index.js
@@ -3,16 +3,25 @@
 const { argv } = require('process');
 
 /**
- * Creates a random password with a specified length and optional numbers.
+ * Creates a random password with specified length and character types.
  * 
  * @param {number} length Length of the password
  * @param {boolean} includeNumbers Whether to include numbers in the password
+ * @param {boolean} includeCapitals Whether to include capital letters in the password
+ * @param {boolean} includeSymbols Whether to include symbols in the password
  * @returns {string} Generated password
  */
-function generatePassword(length, includeNumbers) {
+function generatePassword(length, includeNumbers, includeCapitals, includeSymbols) {
     const lowercase = 'abcdefghijklmnopqrstuvwxyz';
     const numbers = '0123456789';
-    const characters = includeNumbers ? lowercase + numbers : lowercase;
+    const capitals = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const symbols = '!@#$%^&*()_+-=[]{}|;:,.<>?';
+
+    let characters = lowercase;
+    if (includeNumbers) characters += numbers;
+    if (includeCapitals) characters += capitals;
+    if (includeSymbols) characters += symbols;
+
     let password = '';
     for (let i = 0; i < length; i++) {
         const randomChar = characters[Math.floor(Math.random() * characters.length)];
@@ -32,9 +41,11 @@ Options:
     -h, --help                    Display help message
     -l, --length <length>         Set the length of the password (default is 8)
     -n, --numbers                 Include numbers in the password
+    -c, --capitals                Include capital letters in the password
+    -s, --symbols                 Include symbols in the password
 
 Example:
-  password-generator --length 12 --numbers
+  password-generator --length 12 --numbers --capitals --symbols
     `);
 }
 
@@ -46,6 +57,8 @@ Example:
 function handleArguments(userArguments) {
     let length = 8; // Default password length
     let includeNumbers = false;
+    let includeCapitals = false;
+    let includeSymbols = false;
 
     for (let i = 0; i < userArguments.length; i++) {
         switch (userArguments[i]) {
@@ -65,11 +78,19 @@ function handleArguments(userArguments) {
             case '--numbers':
                 includeNumbers = true;
                 break;
+            case '-c':
+            case '--capitals':
+                includeCapitals = true;
+                break;
+            case '-s':
+            case '--symbols':
+                includeSymbols = true;
+                break;
         }
     }
 
     // Generate and display password
-    const password = generatePassword(length, includeNumbers);
+    const password = generatePassword(length, includeNumbers, includeCapitals, includeSymbols);
     console.log(password);
 }
 
